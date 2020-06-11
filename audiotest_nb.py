3d930fa3e42a614fe3d020af595d350b22cd377d
IncUlf <ulf.laemmerhirt@email.de>
2020-06-11 16:38:18 +0200

    Bessere Warteschleife

--- audiotest_nb.py
+++ audiotest_nb.py
@@ -4,10 +4,14 @@ import numpy as np
 
 CHANNELS = 1
 RATE = 44100
-freq = 600
+freq = 220
 CHUNK = 1024
 lastchunk = 0
-def sine(current_time):
+DAUER=1.5 #Sekunden
+VOL=0.2
+
+#def sine(current_time):
+def sine():
     global freq,lastchunk
     length = CHUNK
     factor = float(freq)*2*np.pi/RATE
@@ -16,8 +20,9 @@ def sine(current_time):
     return np.sin(this_chunk*factor)
 
 def get_chunk(): 
-    data  = sine(time.time())
-    return data * 0.1
+#    data  = sine(time.time())
+    data  = sine()
+    return data * VOL
 
 
 def callback(in_data, frame_count, time_info, status):
@@ -30,12 +35,37 @@ p = pyaudio.PyAudio()
 stream = p.open(format=pyaudio.paFloat32,
             channels=CHANNELS,
             rate=RATE,
+            frames_per_buffer=CHUNK,
             output=True,
             stream_callback=callback)
 
 stream.start_stream()
-time.sleep(1)   
+
+akt_time=time.time()
+
+while stream.is_active() and (akt_time+DAUER)>time.time():
+    time.sleep(0.1)   
 
 stream.stop_stream()
+
+freq=440
+stream.start_stream()
+akt_time=time.time()
+
+while stream.is_active() and (akt_time+DAUER)>time.time():
+    time.sleep(0.1)   
+
+stream.stop_stream()
+
+freq=880
+stream.start_stream()
+akt_time=time.time()
+
+while stream.is_active() and (akt_time+DAUER)>time.time():
+    time.sleep(0.1)   
+
+stream.stop_stream()
+
+
 stream.close()
 
